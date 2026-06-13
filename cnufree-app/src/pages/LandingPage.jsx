6fe308586fe466c23914107dfbf2405e7d848f75
Luke Sandigan <lukesandigan986@gmail.com>
2026-06-13 22:38:21 +0800

    Edit Some text

--- cnufree-app/src/pages/LandingPage.jsx
+++ cnufree-app/src/pages/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage() {
                 </div>
 
                 <div className="flex flex-col flex-auto items-center justify-center text-center max-w-2xl">
-                    <h1 className=" font-bold text-[20px] sm:text-[28px] bg-[#fce13487]"> Stay Connected. Go CNU Free! </h1>
+                    <h1 className=" font-bold text-[20px] sm:text-[40px] bg-[#fce13487]"> Stay Connected. Go CNU Free! </h1>
                     <p className=" mt-5 px-5 text-[15px] sm:text-base text-[#9C9C9C]"> The all-in-one student scheduling platform — organize your classes, events, and activities while instantly seeing which friends are available. </p>
                     <button onClick={() => navigate("/SignInPage")} className="hover:bg-[#D6EAC3] transition-all duration-300 ease-in-out m-10 flex gap-2 py-[10px] px-[15px] font-bold rounded-[8px]  bg-[#FCE034] hover:bg-[#5A98E6] " > Get Started     <ChevronLast /> </button>
                 </div>
